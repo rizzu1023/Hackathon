eac76baffd62c1c2f7a559e89c167e355bba0565
Rizzu1023 <mrizwan9022@gmail.com>
2020-01-31 18:56:38 +0530

    routes added

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,10 +9,15 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Route::prefix('admin')->group(function(){
+    Route::get('/dashboard', function () {
+        return view('Admin.index');
+    });
+    Route::resource('/team','TeamController');
+    Route::resource('/event','EventController');
+    Route::resource('/schedule','ScheduleController');
+    Route::resource('/coordinator','CoordinatorController');
 
-Route::get('/home', 'HomeController@index')->name('home');
+});
