991de8ebb5a88f14cb3d7dc1aeb7034d64635d45
Azamn <azamali.shaikh@gmail.com>
2020-01-31 18:54:08 +0530

    migration and controller done

---
+++ database/factories/CoordinatorFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Coordinator;
+use Faker\Generator as Faker;
+
+$factory->define(Coordinator::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
